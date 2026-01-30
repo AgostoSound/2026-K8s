5c78658593f648c5e2556f8d6e1cd9d97fe0d8ad
AgostoSound <agostosound@gmail.com>
2026-01-30 12:05:19 +0100

    add health endpoint to testing.

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,17 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
 from api.views.product import ProductViewSet
 from api.views.reservation import ReservationViewSet
 from api.views.rate import RateViewSet
+from api.health import HealthView
 
 router = DefaultRouter()
 router.register("products", ProductViewSet)
 router.register("reservations", ReservationViewSet)
 router.register("rates", RateViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("health/", HealthView.as_view(), name="health"),
+    path("", include(router.urls)),
+]
